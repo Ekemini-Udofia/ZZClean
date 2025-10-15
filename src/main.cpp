fd7bfc2d3363ca917cef5c6e5a15cf49b4752021
Ekemini-Udofia <ekeminiabasiudofia@gmail.com>
2025-10-15 14:51:53 +0100

    cleared main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,5 @@
-
-
-enum directory {
-  current = 0, recursive = 1
-};
-
+// Update later
 int main(int argc, char* argv[]) {
-  
-  int flag;
-  switch(flag) {
-    case current:
-      // Search the current directory normally
-    case recursive:
-      // Search the current directory recursively
-        
-    default:
-      // Search the current directory normally
-  }
+
   return 0;
 }
