dc1476cdf5b3ef0b95cec936b61972a89c1f12aa
Ekemini-Udofia <ekeminiabasiudofia@gmail.com>
2025-10-16 11:06:42 +0100

    added cxxopts.h cleaner.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
-// Update later
+#include <string>
+#include "cleaner.h"
+#include "cxxopts.hpp"
+
 int main(int argc, char* argv[]) {
+
+  cxxopts::Options options("ZZClean",  "A Basic cli tool to rid build directories of certain files.");
+
+  options.add_options()
+  ("-r,recursive", "Delete from all folder and sub folders within current directory") // a bool parameter
+  ("-f,force", "Force Remove")
+  ("-e,extension", "File extension", cxxopts::value<std::string>())
+  ("-h,help", "Help")
+  ;
+
+  auto result = options.parse(argc, argv);
+
 
   return 0;
 }
